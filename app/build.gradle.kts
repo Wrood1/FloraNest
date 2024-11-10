f0287307c55e80c53dcaa476f121b7826947d2ce
Razo0o <145876906+Razo0o@users.noreply.github.com>
2024-11-10 17:05:27 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.floranestapp"
+    namespace = "com.example.floranest"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.floranestapp"
-        minSdk = 26
+        applicationId = "com.example.floranest"
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,11 +34,12 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
